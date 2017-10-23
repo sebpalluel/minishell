e63e98a74753f0bdeecb3b65546baf64b4581b19
sebpalluel <sebpalluel@free.fr>
2017-10-23 17:11:21 +0200

    initial basic files

---
+++ includes/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/23 17:01:42 by psebasti          #+#    #+#             */
+/*   Updated: 2017/10/23 17:09:02 by psebasti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/includes/libft.h"
+# include "ft_minishell_struct.h"
+# include "ft_minishell_define.h"
+
+#endif
