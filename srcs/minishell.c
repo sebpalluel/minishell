d7477e7ad28e292ad81a30e762e8069ebf7afaa6
psebasti <psebasti@42.fr>
2017-11-09 15:20:01 +0100

    bindirs works

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/23 16:45:28 by psebasti          #+#    #+#             */
-/*   Updated: 2017/11/09 12:13:42 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/11/09 15:19:16 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ t_list		*ft_newenv(char *str)
 	else
 		ENV(env)->value = ft_strdup(tmp);
 	env->next = NULL;
-	printf("env str %s\n", str);
 	return (env);
 }
 
@@ -44,25 +43,50 @@ t_list		*ft_envlist(char **envp)
 	return(env);
 }
 
+char		**ft_bindirs(char **envp)
+{
+	char	**bindirs;
+	char	*tmp;
+	int		i;
+
+	bindirs = NULL;
+	tmp = NULL;
+	if (!envp)
+		return (NULL);
+	i = -1;
+	while (envp && envp[++i])
+	{
+		if (!ft_strncmp(envp[i], "PATH=", 5) && (tmp = ft_strchr(envp[i], '=')))
+		{
+			tmp = ft_strsub(tmp, 1, ft_strlen(tmp) - 1);
+			bindirs = ft_strsplit(tmp, ':');
+			free(tmp);
+			break ;
+		}
+	}
+	return (bindirs);
+}
+
 int			main(int argc, char **argv, char **envp)
 {
 	t_list	*env;
+	char	**bindirs;
 
-	env = ft_envlist(envp);
-	printf("env str after %s\n", ENV(env)->env_str);
+	if (!(env = ft_envlist(envp)) || !(bindirs = ft_bindirs(envp)))
+		return (EXIT_FAILURE);
 	//init_implement_functions(env->content);
 	argc = 0;
 	argv = NULL;
+	int	i;
+	i = -1;
 	while (42)
 	{
-		//init_env(&(data->env), envp, NULL);
-		//data->bin_directories = get_bin_directories(envp);
 		//ft_putstr("$> ");
 		//prompt(data, 0, 0);
 		//if (data->imp_func)
 		//	free_env(data->imp_func);
-		//if (data->bin_directories)
-		//	free_env(data->bin_directories);
+		//if (data->bindirs)
+		//	free_env(data->bindirs);
 		//free(data);
 		//if (argc || argv)
 		//	return (1);
