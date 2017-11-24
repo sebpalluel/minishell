8a275be96c49ee3d3ab23210284b83b7bf0829fb
psebasti <psebasti@42.fr>
2017-11-24 19:25:23 +0100

    first version of echo builtin

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,16 +6,55 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 14:10:14 by psebasti          #+#    #+#             */
-/*   Updated: 2017/11/24 18:11:57 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/11/24 19:22:14 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+void	echo_env(char *str, t_list *env, char nflag)
+{
+	char	*tmp;
+
+	tmp = ft_findenv(env, str + 1);
+	if (nflag)
+		ft_putstr(tmp);
+	else
+		ft_putendl(tmp);
+}
+
 void	ft_echo(void *a)
 {
-	if (a)
-		printf("echo\n");
+	t_sh	*sh;
+	char	**cmds;
+	int		nflag;
+
+	sh = (t_sh *)a;
+	cmds = sh->commands;
+
+	if (*++cmds && !ft_strcmp(*cmds, "-n") && (++cmds || !cmds))
+		nflag = 1;
+	else
+		nflag = 0;
+	if (!*cmds && !nflag)
+		ft_putchar('\n');
+	while (*cmds)
+	{
+		if (*cmds[0] == '$')
+			echo_env(*cmds, sh->env, nflag);
+		else
+		{
+			if (!(*(cmds + 1)) && !nflag)
+				ft_putendl(*cmds);
+			else
+			{
+				ft_putstr(*cmds);
+				if (*(cmds + 1))
+					ft_putchar(' ');
+			}
+		}
+		++cmds;
+	}
 }
 
 void	ft_cd(void *a)
